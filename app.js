7162be416d0ea40757ec2078ab247ca00afcf9a8
guizao1712 <guilherme.leonardi.cruz@escola.pr.gov.br>
2025-09-24 11:07:24 -0300

    ..

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 function criarCartao(categoria, pergunta,   resposta) {
-    console.log(categoria, pergunta,  resposta);
+    let container = document. getElementById ('container')
+    let cartao = document. createElement( 'article')
+    cartao.className = 'cartao'
+    cartao.innerHTML = 
+    <div class="cartao_conteudo">
+                    <h3> MATEMÁTICA básica </h3>
+                    <div class="cartao_conteudo_pergunta">
+                        <p> 1 ) QUANTO É 1+1? </p>
+                    </div>
+                    <div class="cartao_conteudo_pergunta">
+                        <p> 2 </p>
+                    </div>
+                </div>
     }
